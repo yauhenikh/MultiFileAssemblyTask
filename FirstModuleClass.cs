46d08df6872533ae2eee449471b261102ab54348
yauhenikh <khotin.13.nomer@gmail.com>
2019-02-24 16:50:43 +0300

    create files FirstModuleClass.cs and SecondModuleClass.cs

---
+++ FirstModuleClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstModule
+{
+    public class FirstModuleClass
+    {
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello World from first module");
+        }
+    }
+}
---
+++ SecondModuleClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecondModule
+{
+    public class SecondModuleClass
+    {
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello World from second module");
+        }
+    }
+}
